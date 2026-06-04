f8c046ef25fd0676bc3039e8e44d4fa6051fa25c
professionalowo <martin.brandl1307@gmail.com>
2026-06-04 17:47:17 +0200

    feat: refactor App struct methods for improved key event handling and rendering

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,7 +3,12 @@ use std::sync::mpsc;
 use super::worker::{Command, Response};
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
-use ratatui::{DefaultTerminal, Frame};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+    style::{Modifier, Style},
+    widgets::Paragraph,
+};
 
 #[derive(Debug)]
 pub struct App {
@@ -13,10 +18,7 @@ pub struct App {
 }
 
 impl App {
-    pub fn new(
-        command_tx: mpsc::Sender<Command>,
-        output_rx: mpsc::Receiver<Response>,
-    ) -> Self {
+    pub fn new(command_tx: mpsc::Sender<Command>, output_rx: mpsc::Receiver<Response>) -> Self {
         Self {
             should_quit: false,
             command_tx,
@@ -27,20 +29,27 @@ impl App {
     pub fn run(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
         while !self.should_quit {
             terminal.draw(|frame| self.render(frame))?;
-            if matches!(
-                event::read()?,
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char('q'),
-                    ..
-                })
-            ) {
-                self.should_quit = true;
+            if let Event::Key(e) = event::read()? {
+                self.handle_key_event(e);
             }
         }
         Ok(())
     }
 
+    pub fn handle_key_event(&mut self, event: KeyEvent) {
+        match event.code {
+            KeyCode::Char('q') => self.should_quit = true,
+            _ => {}
+        }
+    }
+
     fn render(&mut self, frame: &mut Frame) {
-        frame.render_widget("hello world", frame.area());
+        let [content_area, help_area] =
+            Layout::vertical([Constraint::Min(0), Constraint::Length(1)]).areas(frame.area());
+
+        frame.render_widget("hello world", content_area);
+
+        let help = Paragraph::new("q: Quit").style(Style::default().add_modifier(Modifier::DIM));
+        frame.render_widget(help, help_area);
     }
 }
