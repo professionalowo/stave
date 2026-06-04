5fef765e63c902bfaf92905645bb398bf4f7f60f
professionalowo <martin.brandl1307@gmail.com>
2026-06-04 11:02:21 +0200

    feat: implement App struct with run and render methods for terminal UI

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::sync::mpsc;
+use std::{sync::mpsc, thread};
 use app::App;
 use color_eyre::Result;
 use brew::worker;
-
 mod app;
 mod brew;
+
 
 fn main() -> Result<()> {
     let (main_tx, main_rx) = mpsc::channel::<worker::Command>();
     let (worker_tx, worker_rx) = mpsc::channel::<String>();
-    let worker_handle = std::thread::spawn(move || {
+    let worker_handle = thread::spawn(move || {
         let mut worker = worker::Worker::new(main_rx, worker_tx);
         worker.run();
     });
@@ -18,7 +18,7 @@ fn main() -> Result<()> {
     let mut app = App::new(main_tx.clone(), worker_rx);
     let result = app.run(terminal);
     main_tx.send(worker::Command::Shutdown)?;
-    worker_handle.join().expect("Worker thread panicked");
     ratatui::restore();
+    worker_handle.join().expect("Worker thread panicked");
     result
 }
